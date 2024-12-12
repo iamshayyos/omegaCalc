c30b991b3482c2ac8486cbb3d9a24d562cbf31b0
iamshayyos <shayo20050@gmail.com>
2024-12-12 23:30:09 +0200

    fix: fixed the main module

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from manager import calculate
 def main():
-    inpt=input('Enter an equation use the following operators:\n + - * / \n In addition for those you can use power x^y, avg x@y, max x$y, min x&y, modulo x%y, negative value ~x, and factorial x!.\n write "end" to exit the calculator ')
+    inpt = input('Enter an equation using the following operators:\n'
+                 '+ - * / \n'
+                 'In addition, you can use power x^y, avg x@y, max x$y, min x&y, modulo x%y, '
+                 'negative value ~x, and factorial x!.\n'
+                 'Type "end" to exit the calculator.\n\nEnter an equation: ')
     while inpt!="end":
         calculate(inpt)
-        inpt=input("Enter new equation: ")
+        inpt=input("Enter new equation (or type 'end' to exit): ")
+    print("Calculator exited. Goodbye!")
 if __name__ == "__main__":
     main()
